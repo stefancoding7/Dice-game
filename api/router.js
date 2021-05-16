587a07bd275ab35c0febf2cd3272f332b46f25b6
Stefan Caky <stefan.caky777@gmail.com>
2021-05-16 19:41:52 +0100

    start-client

--- api/router.js
+++ api/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Server is runing changed')
+    res.send('Server is runing changed again')
 });
 
 
