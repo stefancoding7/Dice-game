055e730b0a6de2f6716b5afdcb0cc262360cf6b0
Stefan Caky <stefan.caky777@gmail.com>
2021-05-18 20:53:29 +0100

    changed access control allow origin

--- api/index.js
+++ api/index.js
@@ -13,7 +13,13 @@ var cors = require('cors');
 const app = express();
 app.use(cors())
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
 
 
 const buildPath = path.join(__dirname, '..', 'build');
@@ -29,13 +35,7 @@ const io = socketio(server, {
       }
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-  });
+
   
 
 
