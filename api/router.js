0661777c754aad9000a4ace30a28d57c40dbacbf
Stefan Caky <stefan.caky777@gmail.com>
2021-06-02 19:16:58 +0100

    heroku test 11

--- api/router.js
+++ api/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const path = require('path');
 //for  development
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'build/index.html'));
